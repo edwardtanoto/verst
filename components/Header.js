1004030de6676787689fc31b47be5b5bcc38170a
edwardtanoto <edwardt.dev@gmail.com>
2021-05-07 01:04:52 +0700

    updt

---
+++ components/Header.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import { withRouter } from 'next/router'
+import Link from 'next/link'
+import Hamburger from "./Hamburger";
+
+
+const Header = ({ history }) => {
+  // State of our Menu
+  const [state, setState] = useState({
+    initial: false,
+    clicked: null,
+    menuName: "Menu"
+  });
+  // State of our button
+  const [disabled, setDisabled] = useState(false);
+
+  // Toggle menu
+  const handleMenu = () => {
+    disableMenu();
+    if (state.initial === false) {
+      setState({
+        initial: null,
+        clicked: true,
+        menuName: "Close"
+      });
+    } else if (state.clicked === true) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Menu"
+      });
+    } else if (state.clicked === false) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Close"
+      });
+    }
+  };
+
+  //Determine if out menu button should be disabled
+  const disableMenu = () => {
+    setDisabled(!disabled);
+    setTimeout(() => {
+      setDisabled(false);
+    }, 1200);
+  };
+
+  return (
+    <header>
+      <div className="container">
+        <div className="wrapper">
+          <div className="inner-header">
+            <div className="logo">
+              <Link href="/">verst.</Link>
+            </div>
+            <div className="menu">
+              <button disabled={disabled} onClick={handleMenu}>
+                {state.menuName}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Hamburger state={state} />
+    </header>
+  );
+};
+
+export default withRouter(Header);
---
+++ components/Hero.js
@@ -0,0 +1,15 @@
+const Hero = () => {
+    return (
+        <div className="container">
+            <div className="wrapper">
+                <h5>
+                    <b>Verst, Inc.</b> is a creative, engineer driven, global agency
+            working on advancing the software, advertising and design communities
+            to new heights.
+          </h5>
+            </div>
+        </div>
+    );
+}
+
+export default Hero
