443123330d0374c075d228ea6386ccea73af726e
edwardtanoto <edwardt.dev@gmail.com>
2021-05-16 20:42:12 +0700

    add fonts

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ const Hero = () => {
         <div className="container">
             <div className="wrapper">
                 <h1>
-                    <b>Verst, Inc.</b> is a first-class studio that provides creative, engineer driven, global studio
+                    <b>Verst, Inc.</b> is a first-class remote studio that provides creative, engineer driven, global studio
             working on advancing technology.
           </h1>
             </div>
