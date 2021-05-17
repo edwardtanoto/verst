695fc5fd9f3e68e0222a3dd01bf39c77a0cc1ac3
edwardtanoto <edwardt.dev@gmail.com>
2021-05-18 01:40:59 +0700

    lul

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,8 @@ const Hero = () => {
             <div className="wrapper">
                 <h1>
                     <b>Verst, Inc.</b> is a first-class remote software studio that builds in-house creative, user-driven software.
-          </h1>
+                    Text us at: <a href="sms:+17603382739"> +1 (760) 338-2739</a>
+                </h1>
             </div>
         </div>
     );
