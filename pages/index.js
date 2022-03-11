131d43c18c4d608ef16efc609fc61b33d4b7e376
edwardtanoto <edwardt.dev@gmail.com>
2022-03-11 11:58:25 +0700

    seo friendly now

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,23 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Verst.io | First Class Software</title>
-        <meta name="description" content="Verst is a tech studio designed to build new things." />
+        <title>Verst, Inc.</title>
+        <meta name="description" content="Verst is a holding company focusing to build creative projects in technology to impact society." />
         <link rel="icon" href="/favicon.ico" />
+<meta name="title" content="Verst, Inc."/>
+<meta name="description" content="Verst is a holding company focusing to build creative projects in technology to impact society."/>
+
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="https://www.verst.io/"/>
+<meta property="og:title" content="Verst, Inc."/>
+<meta property="og:description" content="Verst is a holding company focusing to build creative projects in technology to impact society."/>
+<meta property="og:image" content="https://verst.io/cover.png"/>
+
+<meta property="twitter:card" content="summary_large_image"/>
+<meta property="twitter:url" content="https://www.verst.io/"/>
+<meta property="twitter:title" content="Verst, Inc."/>
+<meta property="twitter:description" content="Verst is a holding company focusing to build creative projects in technology to impact society."/>
+<meta property="twitter:image" content="https://verst.io/cover.png"/>
       </Head>
 
 
